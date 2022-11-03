b00e094dc1f9491e004f70439948e6131a759f8f
Mersharch <mersharchdemenya@icloud.com>
2022-11-03 13:43:13 +0000

    configure author routes

---
+++ routes/authors.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const Author = require('../models/author');
+
+const router = express.Router()
+
+//All Authors Route
+router.get('/', async (req, res) => {
+
+    let sOptions={}
+    if(req.query.name != null && req.query.name != ""){
+        sOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(sOptions)
+        res.render('authors/index',{authors:authors})
+    } catch {
+        res.redirect('/')
+    }
+})
+
+
+//New Author Route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() });
+})
+
+
+//Create Author Route
+router.post('/', async (req, res) => {
+
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect('/authors')
+        console.log(newAuthor);
+    } catch {
+        res.render('authors/new', {errorMsg:'Error creating Author'})
+    }
+   
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -7,27 +7,33 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
-
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
+
+//REQUIRING ROUTES
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended:false}))
 
 
 
-
+//DATABSE CONNECTION
 mongoose.connect(process.env.DB_URL, { useNewUrlParser:true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('DB connected successfully'))
 
 
-
+//USING ROUTES
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
+//START SERVER
 app.listen(process.env.PORT || 5000, () => console.log('Server has Started'))
